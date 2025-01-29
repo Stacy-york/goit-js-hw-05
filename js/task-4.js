f811c7a1a80227ba1e926d693bdf81d6a1070753
Stacy York <stacyyyork@gmail.com>
2025-01-29 13:28:09 +0200

    task-4 refactored

--- js/task-4.js
+++ js/task-4.js
@@ -1,7 +1,7 @@
-const getTotalBalanceByGender = (users, gender) => {
-  const filteredUsers = users.filter(user => user.gender === gender);
-  return filteredUsers.reduce((total, user) => total + user.balance, 0);
-};
+const getTotalBalanceByGender = (users, gender) => 
+  users
+    .filter(user => user.gender === gender) 
+    .reduce((total, user) => total + user.balance, 0);
 
 const clients = [
 	{
